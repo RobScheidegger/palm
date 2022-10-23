2fe47a63d7d95aaf0a666ab42b0ff2276cbdcc97
Robert Scheidegger <robert_scheidegger@brown.edu>
2022-10-23 17:06:19 -0400

    add C project scaffolding

---
+++ palm_core/palm_core.cpp
@@ -0,0 +1,12 @@
+﻿// palm_core.cpp : Defines the entry point for the application.
+//
+
+#include "palm_core.h"
+
+using namespace std;
+
+int main()
+{
+	cout << "Hello CMake." << endl;
+	return 0;
+}
