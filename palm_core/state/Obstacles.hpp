1471e50ee817f42c2ab0e552ad3d2b3c0034b6d3
Robert Scheidegger <robert_scheidegger@brown.edu>
2022-11-13 14:15:21 -0500

    improvements to sim and core

--- palm_core/state/Obstacles.hpp
+++ palm_core/state/Obstacles.hpp
@@ -1,6 +1,10 @@
 #pragma once
 
+struct SceneObstacles {
+    std::vector<Obstacle> obstacles;
+};
+
 class Obstacle {
 
 
-}
+};
--- palm_core/state/Planning.hpp
+++ palm_core/state/Planning.hpp
@@ -1,11 +1,2 @@
 #pragma once
 
-#include "RobotState.hpp"
-#include <vector>
-#include "../tracking/Tracking.hpp"
-
-SceneState Delta(const SceneState& state, const std::vector<HandSensorData>& handData);
-
-GestureAction Gesture(const std::vector<HandSensorData>& handData);
-
-std::vector<SceneState>& Plan(const SceneState& state, const SceneState& target);
