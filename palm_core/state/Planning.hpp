40d9ca2bddef247e0fabe901166b86e64ecb9e1d
schornb <55115049+schornb@users.noreply.github.com>
2022-12-12 13:44:40 -0500

    rrt implementation

--- palm_core/state/Planning.hpp
+++ palm_core/state/Planning.hpp
@@ -10,3 +10,4 @@ GestureAction Gesture_Default(const HandDataQueue& handData);
 std::vector<ActualRobotState> Plan_Linear(const ActualRobotState& state, const SceneRobotState& target);
 std::vector<ActualRobotState> Plan_Potential(const ActualRobotState& state, const SceneRobotState& target);
 std::vector<ActualRobotState> Plan_Potential_Gradient(const ActualRobotState& state, const SceneRobotState& target);
+std::vector<ActualRobotState> RRT_Star(const ActualRobotState& state, const SceneRobotState& target);
